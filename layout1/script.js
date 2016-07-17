a64581a881e264f829240621f95f3163722e53b7
shivamswarnkar <ss8464@nyu.edu>
2016-07-17 23:46:17 +0530

    updated contact link, changed some layout

--- layout1/script.js
+++ layout1/script.js
@@ -3,37 +3,43 @@ var set=false;
 
 var name;
 var word;
-function animateName() {
+function animateName(id) {
+	return function(){
+		if(!done){
+			var place = document.getElementsByClassName(id)[0];
+			var result = place.innerHTML;
+			var visible = place.style.visibility
+			if(!set && visible!="hidden"){
+				name = result.slice(1,result.length);
+				word=result[0];
+				set =true;
+			}
+			var curr = result.length;
+			if(curr >= (name.length+1)){
+				result ="";
 
-	if(!done){
-		var place = document.getElementsByClassName("name")[0];
-		var result = place.innerHTML;
-		var visible = place.style.visibility
-		if(!set && visible!="hidden"){
-			name = result.slice(1,result.length);
-			word=result[0];
-			set =true;
-		}
-		var curr = result.length;
-		if(curr >= (name.length+1)){
-			result ="";
+			}
+			else if (curr===0){
+				result = word;
+			}
 
-		}
-		else if (curr===0){
-			result = word;
-		}
+			else{
+				result += name[curr-1];
+			}
 
-		else{
-			result += name[curr-1];
+			document.getElementsByClassName(id)[0].innerHTML=result;
+			if(result==word+name){
+				done=true; 
+				document.getElementsByClassName("text")[0].style.visibility = "visible";
+				var hides = document.getElementsByClassName("hideMe");
+				for(var i=0; i<hides.length; ++i){
+					hides[i].style.visibility = "visible";
+				}
+				
+			}
 		}
-
-		document.getElementsByClassName("name")[0].innerHTML=result;
-		if(result==word+name){
-			done=true; 
-			document.getElementsByClassName("text")[0].style.visibility = "visible";
-		}
-		console.log("here");
 	}
 };
 
-setInterval(animateName, 150);
+var x = animateName("name");
+setInterval(x, 150);
